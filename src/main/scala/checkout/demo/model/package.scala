c32306f8c9ac87838e5fbfe3a5b68d721338bdac
tudorraul <tudorraul@gmail.com>
2016-02-17 15:52:30 +0000

    add Apples and Oranges to application

---
+++ src/main/scala/checkout/demo/model/package.scala
@@ -0,0 +1,9 @@
+package checkout.demo
+
+package object model {
+
+  case class Apple(priceInPence:Int = 60)
+
+  case class Orange(priceInPence:Int = 25)
+
+}
---
+++ src/test/scala/checkout/demo/model/ModelSpec.scala
@@ -0,0 +1,15 @@
+package checkout.demo.model
+
+import org.specs2.mutable.Specification
+
+class ModelSpec extends Specification {
+
+  "An apple must cost 60p" >> {
+    Apple().priceInPence mustEqual 60
+  }
+
+  "An orange must cost 25p each" >> {
+    Orange().priceInPence mustEqual 25
+  }
+
+}
