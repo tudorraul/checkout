3c4ade633b69397a47d6bf6723ddc72e40293fd8
tudorraul <tudorraul@gmail.com>
2016-02-17 16:03:03 +0000

    Checkout System can compute the price of a list of Items

--- src/main/scala/checkout/demo/CheckoutSystem.scala
+++ src/main/scala/checkout/demo/CheckoutSystem.scala
@@ -1,5 +1,9 @@
 package checkout.demo
 
+import checkout.demo.model.Item
+
 class CheckoutSystem() {
+
+  def computePrice(items: List[Item]): Int = items.map(_.priceInPence).sum
 
 }
--- src/test/scala/checkout/demo/CheckoutSystemSpec.scala
+++ src/test/scala/checkout/demo/CheckoutSystemSpec.scala
@@ -1,5 +1,6 @@
 package checkout.demo
 
+import checkout.demo.model.{Item, Orange, Apple}
 import org.specs2.mutable.Specification
 
 class CheckoutSystemSpec extends Specification{
@@ -7,6 +8,11 @@ class CheckoutSystemSpec extends Specification{
   "Checkout system must" >> {
     "exist" >> {
       new CheckoutSystem() must haveClass[CheckoutSystem]
+    }
+
+    "compute the price of a list of items" >> {
+      val items: List[Item with Product with Serializable] = List(Apple(), Apple(), Orange(), Apple())
+      new CheckoutSystem().computePrice(items) mustEqual 205
     }
   }
 
