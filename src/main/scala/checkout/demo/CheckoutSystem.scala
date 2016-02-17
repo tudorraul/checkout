f0cbcba3b5356c2bf657d3e4234ecd6bdfa8d04c
tudorraul <tudorraul@gmail.com>
2016-02-17 16:57:19 +0000

    add support for two-for-one Apples offer

--- src/main/scala/checkout/demo/CheckoutSystem.scala
+++ src/main/scala/checkout/demo/CheckoutSystem.scala
@@ -1,9 +1,17 @@
 package checkout.demo
 
-import checkout.demo.model.Item
+import checkout.demo.model.{Apple, Item, Orange}
 
 class CheckoutSystem() {
 
   def computePrice(items: List[Item]): Int = items.map(_.priceInPence).sum
+
+  def computePriceWithOffer(items: List[Item]): Int =
+    items.groupBy(i => i).map {
+      case (itemType: Apple, apples) =>
+        itemType.priceInPence * ((apples.size + 1) / 2)
+
+      case (itemType: Orange, oranges) => ???
+    }.sum
 
 }
--- src/test/scala/checkout/demo/CheckoutSystemSpec.scala
+++ src/test/scala/checkout/demo/CheckoutSystemSpec.scala
@@ -11,8 +11,29 @@ class CheckoutSystemSpec extends Specification{
     }
 
     "compute the price of a list of items" >> {
-      val items: List[Item with Product with Serializable] = List(Apple(), Apple(), Orange(), Apple())
+      val items = List(Apple(), Apple(), Orange(), Apple())
       new CheckoutSystem().computePrice(items) mustEqual 205
+    }
+  }
+
+  "Checkout system must compute the price of items on offer" >> {
+
+    "Apples offers" >> {
+
+      "one Apple for the price of one" >> {
+        val items = List.fill(1)(Apple())
+        new CheckoutSystem().computePriceWithOffer(items) mustEqual 60
+      }
+
+      "two Apples for the price of one" >> {
+        val items = List.fill(2)(Apple())
+        new CheckoutSystem().computePriceWithOffer(items) mustEqual 60
+      }
+
+      "three Apples for the price of two" >> {
+        val items = List.fill(3)(Apple())
+        new CheckoutSystem().computePriceWithOffer(items) mustEqual 120
+      }
     }
   }
 
